30892f705a45a2cb046f41f858095cb2c07d7c27
JulianReid541 <reidjulian@outlook.com>
2020-07-01 12:28:26 -0700

    added code to check time and round up/down depending on the time. Also auto updates the background of the time

--- GetAirWristBandSorter/wwwroot/js/site.js
+++ GetAirWristBandSorter/wwwroot/js/site.js
@@ -1,4 +1,58 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function init() {
+    let date = new Date();
+    let hour = addZero(date.getHours());
+    let minute = addZero(date.getMinutes());
 
-// Write your JavaScript code.
+    let combinedTime = hour + ":" + minute;
+
+    let roundedTime = RoundUp(combinedTime);
+
+    document.getElementById(roundedTime).style.backgroundColor = "GoldenRod";
+}
+
+function RoundUp(time) {
+
+    let [hours, minutes] = time.split(':');
+    hours = parseInt(hours);
+    minutes = parseInt(minutes);
+
+    let rMinute = roundMinute(minutes);
+
+    let rHour = roundHour(hours, rMinute);
+
+    let roundedTime = rHour + ":" + rMinute;
+
+
+    return roundedTime;
+}
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function roundMinute(m) {
+    if (m <= 30) {
+        return 30;
+    }
+    else if (m > 30) {
+        return 0;
+    }
+}
+
+function roundHour(h, m) {
+    const d = new Date();
+    if (m != 0) {
+        return addZero(d.getHours());
+    }
+    else {
+        return addZero(d.getHours + 1);
+    }
+}
+
+
+Window.Onload = init();
+
+setInterval(init, 1000);
