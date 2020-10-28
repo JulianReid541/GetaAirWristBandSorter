2096bde21fe73ae495b5a2a4a1811c69949c0e6e
JulianReid541 <reidjulian@outlook.com>
2020-10-28 15:48:34 -0700

    going back to old code

--- GetAirWristBandSorter/Controllers/HomeController.cs
+++ GetAirWristBandSorter/Controllers/HomeController.cs
@@ -1,10 +1,7 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using GetAirWristBandSorter.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace GetAirWristBandSorter.Controllers
 {
